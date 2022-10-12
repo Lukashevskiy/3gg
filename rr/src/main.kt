330ce68a15dccb52feef392b9b432cc72bf822f6
dmitriy <dim4ik-super-klass@mail.ru>
2022-10-12 14:13:51 +0800

    changes

--- rr/src/main.kt
+++ rr/src/main.kt
@@ -3,22 +3,19 @@ import java.util.*
 
 fun main() {
     val filename = "/home/dmitriy/Downloads/roles.txt"
-    val flags = arrayOf("textLines:", "roles:")
     var l = 0
     val roles = mutableListOf<String>()
-    val texts = mutableListOf<String>()
     val dependencies = mutableMapOf<String, String>()
-    var i = 0
+    var i = 1
     File(filename).forEachLine { line ->
         when(l){
             0 -> roles + line
             1 ->{
                 val sep = line.indexOf(':')
                 val name = line.substring(0, sep)
-                if(name in dependencies)
-                    dependencies[name] += "\t$i) ${line.substring(sep + 2)}\n"
-                else
+                if(name !in dependencies)
                     dependencies[name] = ""
+                dependencies[name] += "\t$i) ${line.substring(sep + 2)}\n"
                 i++
             }
         }
