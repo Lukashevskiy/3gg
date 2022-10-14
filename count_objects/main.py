df2188cdd8d85b7991d671befdfe9e92db255b32
dmitriy <dim4ik-super-klass@mail.ru>
2022-10-14 09:10:13 +0800

    intial

--- count_objects/main.py
+++ count_objects/main.py
@@ -5,10 +5,17 @@ from matplotlib import use
 from skimage.measure import label
 use('Qt5Agg')
 
-data = np.load('ps.npy').astype('uint8')
+data = np.load('./count_objects/ps.npy').astype('uint8')
 data_labeled = label(data)
 print(f'number of figures on image: {data_labeled.max()}')
-data_erosied = binary_erosion(data)
-plt.figure()
-plt.imshow(data_erosied)
-plt.show()
+data_erosied = binary_erosion(data, np.array([[1,1,1],[0,1,0],[1,1,1]]))
+data_erosied_labeled = label(data_erosied)
+#plt.figure()
+print(f'number of rectangles on image: {data_erosied_labeled.max()}')
+print(f'number of other objects on image: {data_labeled.max() - data_erosied_labeled.max()}')
+#plt.subplot(211)
+#plt.imshow(data)
+#plt.subplot(212)
+#plt.imshow(data_erosied)
+#plt.show()
+
